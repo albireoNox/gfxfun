ebc1f99e1f79de255f92564088a2f2fdfbf5b8f7
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-06-04 13:54:22 -0700

    Hello world with other files.

---
+++ test/d3dSetup.cpp
@@ -0,0 +1,8 @@
+#include "d3dSetup.h"
+
+#include <iostream>
+
+void foo()
+{
+	std::cout << "This is a test." << std::endl;
+}
--- test/main.cpp
+++ test/main.cpp
@@ -1,8 +1,11 @@
+#include "d3dSetup.h"
+
 #include <iostream>
 
 int main()
 {
 	std::cout << "Hello!" << std::endl;
+	foo();
 	int i;
 	std::cin >> i;
 	return i;
