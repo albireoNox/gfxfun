24f432d9ac6591f7eb2a076a12742e45c88944d7
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-06-05 14:42:32 -0700

    Try logging adapters

--- test/main.cpp
+++ test/main.cpp
@@ -4,19 +4,34 @@
 #include <vector>
 
 using Microsoft::WRL::ComPtr;
+using namespace std;
+
+void log_adapters(vector<ComPtr<IDXGIAdapter>> & adapters)
+{
+	cout << "Logging " << adapters.size() << " adapters." << endl;
+	for (uint i = 0; i < adapters.size(); i++)
+	{
+		cout << "*** Adapter " << i << " ***" << endl;
+		IDXGIAdapter * adapter = adapters.at(i).Get();
+		DXGI_ADAPTER_DESC description;
+		hrThrowIfFailed(adapter->GetDesc(&description));
+		wcout << description.Description << endl;
+	}
+}
 
 int run_main()
 {
-	std::cout << "Hello!" << std::endl;
+	cout << "Hello!" << endl;
 
 	ComPtr<IDXGIFactory> factory;
 	initDxgiFactory(factory);
-	std::vector<ComPtr<IDXGIAdapter>> adapters;
+	vector<ComPtr<IDXGIAdapter>> adapters;
 	populateAdapterList(factory.Get(), adapters);
+	log_adapters(adapters);
 
 	foo();
 	int i;
-	std::cin >> i;
+	cin >> i;
 
 	return i;
 }
