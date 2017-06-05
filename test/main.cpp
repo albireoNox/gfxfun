4f13098ce0dee2a091ed2dd85ee13c0b5b4b70d0
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-06-04 22:06:29 -0700

    Remove useless crap.
    Leave useful memory leak detection crap.

--- test/main.cpp
+++ test/main.cpp
@@ -1,41 +1,12 @@
 #include "common.h"
 #include "d3dSetup.h"
 #include <iostream>
-#include <vector>
 
 using Microsoft::WRL::ComPtr;
-using namespace std;
-
-class Classy
-{
-public:
-	Classy(int i);
-	~Classy();
-protected: 
-	int i;
-};
-
-Classy::Classy(int i) : i(i) { }
-
-Classy::~Classy()
-{
-	cout << "Destroying classy number " << this->i << "." << endl;
-}
 
 int main()
 {	
 	std::cout << "Hello!" << std::endl;
-
-	{
-		vector<Classy> * classies = new vector<Classy>();
-		classies->push_back(Classy(1));
-		classies->push_back(2);
-		Classy * classy = new Classy(3);
-		classies->push_back(*classy);
-		cout << classies->size() << endl;
-		delete classy;
-		delete classies;
-	}
 
 	ComPtr<IDXGIFactory> factory;
 	initDxgiFactory(factory);
