0fb5836ad33d4f8ef90c5146654f2792a0460caf
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-06-04 21:47:58 -0700

    Um cool vector

--- test/main.cpp
+++ test/main.cpp
@@ -1,12 +1,39 @@
 #include "common.h"
 #include "d3dSetup.h"
 #include <iostream>
+#include <vector>
 
 using Microsoft::WRL::ComPtr;
+using namespace std;
+
+class Classy
+{
+public:
+	Classy(int i);
+	~Classy();
+protected: 
+	int i;
+};
+
+Classy::Classy(int i) : i(i) { }
+
+Classy::~Classy()
+{
+	cout << "Destroying classy number " << this->i << "." << endl;
+}
 
 int main()
-{
+{	
 	std::cout << "Hello!" << std::endl;
+
+	{
+		vector<Classy> classies;
+		classies.push_back(Classy(1));
+		classies.push_back(2);
+		Classy * classy = new Classy(3);
+		classies.push_back(*classy);
+		cout << classies.size() << endl;
+	}
 
 	ComPtr<IDXGIFactory> factory;
 	initDxgiFactory(factory);
