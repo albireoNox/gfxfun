7402e19736a0a14ffc279e3c8960137d89fa3c7f
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-06-05 22:08:31 -0700

    Put link dependency on dxgi.lib in project file instead of code.

--- test/common.h
+++ test/common.h
@@ -5,8 +5,5 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 
-// Need this or else linking directX will fail (for some reason). 
-#pragma comment(lib, "dxgi.lib") 
-
 // Common includes
 #include "types.h"
