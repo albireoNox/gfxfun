46060a8022bc0a96d648af20facd1e07fcd47499
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-07-09 11:37:37 -0700

    Missing reference.

--- lib/common.h
+++ lib/common.h
@@ -9,5 +9,6 @@
 #pragma comment(lib, "D3D12.lib")
 #pragma comment(lib, "D3D11.lib")
 #pragma comment(lib, "D2d1.lib")
+#pragma comment(lib, "D3DCompiler.lib")
 #pragma comment(lib, "dxgi.lib")
 #pragma comment(lib, "dwrite.lib")
