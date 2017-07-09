d20b6570c1dc25d9dc088a0ce92dd6aac6eb1d6c
Nicholas Wilson <albireoNox@users.noreply.github.com>
2017-07-09 11:26:27 -0700

    Turns out the linker doesn't like static field init in the header.

--- lib/geometry.h
+++ lib/geometry.h
@@ -4,8 +4,6 @@
 #include <DirectXMath.h>
 #include <vector>
 
-/// VERTEX ///
-
 struct Vertex
 {
 	DirectX::XMFLOAT3 pos;
@@ -15,13 +13,3 @@ struct Vertex
 	static const D3D12_INPUT_LAYOUT_DESC LAYOUT_DESC;
 };
 
-const std::vector<D3D12_INPUT_ELEMENT_DESC> Vertex::LAYOUT_FIELDS(
-{
-	{ "POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0,
-	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-	{ "COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, sizeof(DirectX::XMFLOAT3),
-	D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA, 0 },
-});
-
-const D3D12_INPUT_LAYOUT_DESC Vertex::LAYOUT_DESC =
-{ Vertex::LAYOUT_FIELDS.data(), Vertex::LAYOUT_FIELDS.size() };
